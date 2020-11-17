9fd610c452504c17ee91da80a4ac4fbabcdae47b
samumartinf <samumartinfrias@gmail.com>
2020-11-17 20:01:20 +0000

    Initial commit of Postgres with Django and Docker

---
+++ Docker-Django/app/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/SamuelMartin/Desktop/python/Docker-Django/Docker-Django/app/env/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
